850692e1c7449ae83f22c2bce48c8ca8d32af45e
miskyy7507 <misiulek0w0@gmail.com>
2024-06-03 13:31:50 +0200

    Add message for user to add tasks when list is empty

--- app/src/main/java/com/mm/nexttasks/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mm/nexttasks/ui/home/HomeFragment.kt
@@ -3,8 +3,8 @@ package com.mm.nexttasks.ui.home
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mm.nexttasks.MainApp
@@ -58,8 +58,8 @@ class HomeFragment : Fragment() {
 
     private fun setUpTaskListModels() {
         val tasks = taskDao!!.getAll()
-        if (tasks.isEmpty()) {
-            Toast.makeText(requireContext(), "Empty task list", Toast.LENGTH_LONG).show()
+        if (tasks.isNotEmpty()) {
+            binding.noTasksTip.visibility = GONE
         }
         for (task in tasks) {
             taskModels.add(task)
