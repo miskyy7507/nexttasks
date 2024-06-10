39564bcef1f1269c9449c5782ebd4d1396572ec3
miskyy <misiulek0w0@gmail.com>
2024-06-10 22:10:50 +0200

    part of implementing calendar view

--- app/src/main/java/com/mm/nexttasks/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/mm/nexttasks/ui/calendar/CalendarFragment.kt
@@ -5,56 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.children
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import com.kizitonwose.calendar.core.daysOfWeek
+import com.kizitonwose.calendar.view.MonthDayBinder
+import com.kizitonwose.calendar.view.ViewContainer
 import com.mm.nexttasks.R
+import com.mm.nexttasks.databinding.CalendarDayViewBinding
+import com.mm.nexttasks.databinding.FragmentCalendarBinding
+import java.time.DayOfWeek
+import java.time.YearMonth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CalendarFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CalendarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentCalendarBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentCalendarBinding.inflate(inflater, container, false)
+
+        val currentMonth = YearMonth.now()
+        val startMonth = currentMonth.minusMonths(100)
+        val endMonth = currentMonth.plusMonths(100)
+        val daysOfWeek = daysOfWeek(firstDayOfWeek = firstDayOfWeekFromLocale())
+
+        configureBinders(daysOfWeek)
+        binding.calendar.setup(startMonth, endMonth, daysOfWeek.first())
+        binding.calendar.scrollToMonth(currentMonth)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalendarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CalendarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun configureBinders(daysOfWeek: List<DayOfWeek>) {
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            val dayTextView = CalendarDayViewBinding.bind(view).dayText
+        }
+
+        binding.calendar.dayBinder = object : MonthDayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+
+            override fun bind(container: DayViewContainer, data: CalendarDay) {
+                container.dayTextView.text = data.date.dayOfMonth.toString()
             }
+        }
     }
 }
