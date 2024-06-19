7a7c8eb2306ba5718a7a8bc0d4b721b26d32cde3
miskyy <misiulek0w0@gmail.com>
2024-06-19 22:59:18 +0200

    Small changes to the code structure

--- app/src/main/java/com/mm/nexttasks/MainActivity.kt
+++ app/src/main/java/com/mm/nexttasks/MainActivity.kt
@@ -13,14 +13,20 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.mm.nexttasks.databinding.ActivityMainBinding
 import com.mm.nexttasks.db.entities.TaskList
 import com.mm.nexttasks.ui.taskList.TaskListFragment
 
+private const val NAV_MENU_ADD_NEW_TASK_LIST_ITEM_ID = -1
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
 
     private var database = DatabaseProvider.getDatabase(this)
     private var taskListDao = database.taskListDao()
@@ -31,7 +37,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val toolbar = binding.appBarMain.toolbar
+        toolbar = binding.appBarMain.toolbar
+        drawerLayout = binding.drawerLayout
+        navView = binding.navView
 
         setSupportActionBar(toolbar)
 
@@ -40,24 +48,28 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-
         val toggle = ActionBarDrawerToggle(
             this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
-
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
+        populateNavMenu();
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun populateNavMenu() {
         val taskListsList = taskListDao.getAll()
 
         val allTaskListsMenuItem = navView.menu.add(1, 0, Menu.NONE, getText(R.string.task_list_show_all)).setIcon(R.drawable.checklist_40dp)
         for (list in taskListsList) {
             navView.menu.add(1, list.taskListId.toInt(), Menu.NONE, list.name)
         }
-        navView.menu.add(2, 9999, Menu.NONE, getString(R.string.add_new_task_list)).setIcon(R.drawable.plus_32)
+        navView.menu.add(2, NAV_MENU_ADD_NEW_TASK_LIST_ITEM_ID, Menu.NONE, getString(R.string.add_new_task_list)).setIcon(R.drawable.plus_32)
         navView.menu.setGroupCheckable(1, true, true)
         allTaskListsMenuItem.setChecked(true)
         supportActionBar?.title = getText(R.string.task_list_show_all)
@@ -68,7 +80,7 @@ class MainActivity : AppCompatActivity() {
 
         // Set up the click listener for the navigation drawer items
         navView.setNavigationItemSelectedListener { menuItem ->
-            if (menuItem.itemId == 9999) {
+            if (menuItem.itemId == NAV_MENU_ADD_NEW_TASK_LIST_ITEM_ID) {
                 val alertDialog = AlertDialog.Builder(binding.root.context)
                 alertDialog.setTitle(getString(R.string.add_new_task_list))
 
@@ -79,10 +91,10 @@ class MainActivity : AppCompatActivity() {
                 alertDialog.setPositiveButton(getText(R.string.add)) { dialog, _ ->
                     val newTaskListName = input.text.toString()
                     if (newTaskListName.isNotBlank()) {
-                        navView.menu.removeItem(9999)
+                        navView.menu.removeItem(NAV_MENU_ADD_NEW_TASK_LIST_ITEM_ID)
                         val newlyInsertedTaskListId = taskListDao.insert(TaskList(0, newTaskListName))
                         navView.menu.add(Menu.FIRST, newlyInsertedTaskListId.toInt(), Menu.NONE, newTaskListName)
-                        navView.menu.add(2, 9999, Menu.NONE, getString(R.string.add_new_task_list))
+                        navView.menu.add(2, NAV_MENU_ADD_NEW_TASK_LIST_ITEM_ID, Menu.NONE, getString(R.string.add_new_task_list))
                         dialog.dismiss()
                     }
                 }
@@ -105,10 +117,5 @@ class MainActivity : AppCompatActivity() {
             drawerLayout.close()
             true
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
